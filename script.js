4ed59b256e476b9bc82bc8ea94b82bf7f75cbaad
Zeynep35 <38433779+Zeynep35@users.noreply.github.com>
2024-11-12 23:31:56 +0300

    Yapılacak listesinin ilk dosyalarını yükledim

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";  // Burada "×" çarpı işaretini ekliyorsun, "\u00dy" değil
+        li.appendChild(span);
+    }
+    inputBox.value = '';  // Değer ataması burada yapılmalı
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
